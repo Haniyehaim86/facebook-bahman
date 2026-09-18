fd8c17b81c17c6e20fed7bb2b47f23d2942afc0c
haniyeh <haniyehprogram@gmail.com>
2026-09-18 08:03:57 -0700

    add change file

--- facebook/asgi.py
+++ facebook/asgi.py
@@ -7,10 +7,3 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facebook.settings')
-
-application = get_asgi_application()
